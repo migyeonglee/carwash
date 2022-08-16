87f2a2dea9201c4f26f0879e2f667375c8f40113
enter-the-code <ssoonngg53@naver.com>
2022-08-16 18:59:54 +0900

    add carwash_data

--- ProjectFil/app.js
+++ ProjectFil/app.js
@@ -1,7 +1,7 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 const path = require('path');
-const port=8000;
+const port = 8000;
 
 // const file=require("/Users/song-giyeong/car_washer/carwash/carwash_data.json");
 
@@ -9,12 +9,36 @@ app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
 app.use("/static", express.static("static"))
 app.use("/data", express.static("data"))
 
+var file=require("./data/carwash_seoul.json")
+
+let ranking=[]
+for (let index = 0; index < file.length; index++) {
+    ranking.push({
+        "like":0,
+        "dislike":0
+    })
+    
+}
+
+console.log(ranking)
+
+let fs = require("fs")
+fs.writeFile("./data/ranking_seoul.json", JSON.stringify(ranking), function (err) {
+    if (err) throw err;
+    console.log('complete');
+}
+);
 
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("template");
 })
-app.listen(port,()=>{
-    console.log("server open: ",port);
+
+app.get("/ranking", (req, res) => {
+    res.render("ranking",file);
+})
+
+app.listen(port, () => {
+    console.log("server open: ", port);
 })
