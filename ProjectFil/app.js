d93f69b9cee8a556b14dfa266ae2a582a5206e9f
enter-the-code <ssoonngg53@naver.com>
2022-08-16 15:17:22 +0900

    fixed bug

--- ProjectFil/app.js
+++ ProjectFil/app.js
@@ -6,7 +6,7 @@ const port=8000;
 // const file=require("/Users/song-giyeong/car_washer/carwash/carwash_data.json");
 
 app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
-app.set('views', path.join(__dirname, 'views')); //폴더, 폴더경로 지정
+app.use("/static", express.static("static"))
 
 
 
