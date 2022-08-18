35284010b210ac32eaa687b263b1e3655ebbbe68
enter-the-code <ssoonngg53@naver.com>
2022-08-19 00:53:40 +0900

    push last ranking page

--- ProjectFil/app.js
+++ ProjectFil/app.js
@@ -8,16 +8,37 @@ const port = 8000;
 app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
 app.use("/static", express.static("static"))
 app.use("/data", express.static("data"))
+let ranking_index={"서울특별시":[],"부산광역시":[],"대구광역시":[],"인천광역시":[],"울산광역시":[],"대전광역시":[]}
 
-var file=require("./data/carwash_daejun.json")
-
-let ranking={};
-var region = ["seoul","busan","daegu","inchun","ulsan","daejun"];
-for(let index=0;index<region.length;index++){
-    var file=require(`./data/carwash_${region[index]}.json`);
-    ranking[region[index]]=file;
+let rating={};
+var region = ["서울특별시","부산광역시","대구광역시","인천광역시","울산광역시","대전광역시"];
+for (let index = 0; index < region.length; index++) {
+    let file=require(`./data/ranking_${region[index]}.json`);
+    for (let i = 0; i< file.length; i++) {
+        
+        ranking_index[region[index]].push(i);
+        
+    }
+    
+}
+var car_washer_name={"서울특별시":[],"부산광역시":[],"대구광역시":[],"인천광역시":[],"울산광역시":[],"대전광역시":[]};
+const a=()=>{
+    for(let index=0;index<region.length;index++){
+        let file=require(`./data/ranking_${region[index]}.json`);
+        let file2=require(`./data/carwash_${region[index]}.json`);
+        rating[region[index]]=file;
+        // console.log(file2.length)
+        for(let i=0;i<file2.length;i++){
+            car_washer_name[region[index]].push(file2[i]["사업장명"]);
+        }
+    }
 }
 
+a()
+
+playAlert = setInterval(function() {
+    a()
+}, 3600000);
 
 
 
@@ -26,7 +47,9 @@ app.get("/", (req, res) => {
 })
 
 app.get("/ranking", (req, res) => {
-    res.render("ranking",file);
+    // console.log(rating);
+    // console.log(car_washer_name);
+    res.render("ranking",{region:region,rating:rating,car_washer_name:car_washer_name,ranking_index:ranking_index});
 })
 
 app.listen(port, () => {
