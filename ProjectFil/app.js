953cda0f1eee87b9ba13aca991c7e2087137693e
enter-the-code <ssoonngg53@naver.com>
2022-08-19 18:32:13 +0900

    fixed disign

--- ProjectFil/app.js
+++ ProjectFil/app.js
@@ -3,13 +3,10 @@ const app = express();
 const path = require('path');
 const port = 8000;
 
-// const file=require("/Users/song-giyeong/car_washer/carwash/carwash_data.json");
-
 app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
 app.use("/static", express.static("static"))
 app.use("/data", express.static("data"))
 let ranking_index={"서울특별시":[],"부산광역시":[],"대구광역시":[],"인천광역시":[],"울산광역시":[],"대전광역시":[]}
-
 let rating={};
 var region = ["서울특별시","부산광역시","대구광역시","인천광역시","울산광역시","대전광역시"];
 for (let index = 0; index < region.length; index++) {
@@ -33,9 +30,7 @@ const a=()=>{
         }
     }
 }
-
 a()
-
 playAlert = setInterval(function() {
     a()
 }, 3600000);
@@ -47,7 +42,7 @@ app.get("/", (req, res) => {
 })
 
 app.get("/ranking", (req, res) => {
-    // console.log(rating);
+    console.log(ranking_index);
     // console.log(car_washer_name);
     res.render("ranking",{region:region,rating:rating,car_washer_name:car_washer_name,ranking_index:ranking_index});
 })
